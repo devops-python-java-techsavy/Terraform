f0657d1182eb0f6dca331d293753153c0f17cedb
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:48:27 +0530

    Update main.tf

--- basicterraform/my-aws-infrastructure/main.tf
+++ basicterraform/my-aws-infrastructure/main.tf
@@ -1,1 +1,34 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+module "ec2_instance" {
+  source        = "./modules/ec2_instance"
+  instance_type = var.instance_type
+}
+
+module "load_balancer" {
+  source = "./modules/load_balancer"
+}
+
+module "database" {
+  source             = "./modules/database"
+  db_instance_class = var.db_instance_class
+  db_username       = var.db_username
+  db_password       = var.db_password
+}
+
+output "instance_id" {
+  value = module.ec2_instance.instance_id
+}
+
+output "load_balancer_dns" {
+  value = module.load_balancer.lb_dns_name
+}
+
+output "database_endpoint" {
+  value = module.database.db_endpoint
+}
 
