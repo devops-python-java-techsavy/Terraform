caf4896433106d536885ed7192e663c49d5c2aa2
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:47:44 +0530

    Update main.tf

--- basicterraform/my-aws-infrastructure/modules/load_balancer/main.tf
+++ basicterraform/my-aws-infrastructure/modules/load_balancer/main.tf
@@ -1,1 +1,9 @@
+resource "aws_lb" "my_load_balancer" {
+  name               = "my-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-12345678", "subnet-87654321"]  # Replace with your subnet IDs
+
+  enable_deletion_protection = false
+}
 
