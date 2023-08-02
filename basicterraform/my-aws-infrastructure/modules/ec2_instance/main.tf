d203b1b846f14a275c9aaaf21655150cc0fc171e
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:47:27 +0530

    Update main.tf

--- basicterraform/my-aws-infrastructure/modules/ec2_instance/main.tf
+++ basicterraform/my-aws-infrastructure/modules/ec2_instance/main.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "my_instance" {
+  ami           = "ami-12345678"  # Replace with the desired AMI ID
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
 
