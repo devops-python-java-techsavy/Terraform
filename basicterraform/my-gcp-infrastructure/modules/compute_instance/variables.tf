348e5dcfa9a61dd7d163c4bc1f8b2e3f56836c27
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:57:17 +0530

    Create variables.tf

---
+++ basicterraform/my-gcp-infrastructure/modules/compute_instance/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "The GCP instance type"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
