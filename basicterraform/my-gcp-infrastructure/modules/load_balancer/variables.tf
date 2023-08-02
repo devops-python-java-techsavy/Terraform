16cd2d21d6da70b77a1421e6a7fe043951bd84a0
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:58:53 +0530

    Create variables.tf

---
+++ basicterraform/my-gcp-infrastructure/modules/load_balancer/variables.tf
@@ -0,0 +1,9 @@
+variable "load_balancer_name" {
+  description = "The name of the load balancer"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
