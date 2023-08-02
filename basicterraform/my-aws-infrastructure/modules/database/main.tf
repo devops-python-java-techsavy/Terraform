277569c7e2ed5f773a33bb48efaca92f26c03546
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:48:06 +0530

    Update main.tf

--- basicterraform/my-aws-infrastructure/modules/database/main.tf
+++ basicterraform/my-aws-infrastructure/modules/database/main.tf
@@ -1,1 +1,16 @@
+resource "aws_db_instance" "my_database" {
+  allocated_storage    = 20
+  storage_type        = "gp2"
+  engine              = "mysql"
+  instance_class      = var.db_instance_class
+  name                = "mydb"
+  username            = var.db_username
+  password            = var.db_password
+
+  skip_final_snapshot = true
+
+  tags = {
+    Name = "MyDB"
+  }
+}
 
