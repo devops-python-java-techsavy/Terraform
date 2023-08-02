cf86cf57b426c52cfa1cc6bfba10e9e99ad85007
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 21:46:06 +0530

    Update variables.tf

--- basicterraform/my-aws-infrastructure/variables.tf
+++ basicterraform/my-aws-infrastructure/variables.tf
@@ -1,1 +1,8 @@
-
+# Define your variable
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {}
+variable "instance_type" {}
+variable "db_instance_class" {}
+variable "db_username" {}
+variable "db_password" {}
