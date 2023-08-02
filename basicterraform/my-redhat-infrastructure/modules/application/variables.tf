792b84f7d74571aa59cfa7a958fb53fb88546b62
devops-python-java-techsavy <141257948+devops-python-java-techsavy@users.noreply.github.com>
2023-08-02 22:37:13 +0530

    Create variables.tf

---
+++ basicterraform/my-redhat-infrastructure/modules/application/variables.tf
@@ -0,0 +1,9 @@
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+}
+
+variable "app_port" {
+  description = "Port on which the application will run"
+  type        = number
+}
