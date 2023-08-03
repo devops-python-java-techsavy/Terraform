a234c3e907a96d1be1c0eeab229c887ad01f9f78
Sujith Uday <sujithuday29@gmail.com>
2023-08-03 20:18:01 +0530

    Modified output.tf

--- basicterraform/my-aws-infrastructure/outputs.tf
+++ basicterraform/my-aws-infrastructure/outputs.tf
@@ -1,1 +1,11 @@
+output "instance_id" {
+  value = module.ec2_instance.instance_id
+}
 
+output "load_balancer_dns" {
+  value = module.load_balancer.lb_dns_name
+}
+
+output "database_endpoint" {
+  value = module.database.db_endpoint
+}
